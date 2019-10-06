ecc3827cc52df1601822e5389fe36709115df931
bhavyankaria <bhavya.karia@go-upwards.com>
2019-10-06 18:11:42 +0530

    initial code to fetch contact details without permission check

---
+++ FetchUserContacts/README.md
@@ -0,0 +1,12 @@
+# FetchUserContact
+
+Step by step guide for fetching list of contacts whose data matches all or part of a search string.
+
+Different techniques used:
+1. Match contact names
+2. Match a specific type of data, such as a phone number
+3. Match any type of data
+4. Fetch complete list of contacts
+
+
+
--- FetchUserContacts/app/src/main/java/com/bhavyakaria/fetchusercontacts/MainActivity.java
+++ FetchUserContacts/app/src/main/java/com/bhavyakaria/fetchusercontacts/MainActivity.java
@@ -1,14 +1,72 @@
 package com.bhavyakaria.fetchusercontacts;
 
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView contact_list_view;
+    Map<String, String> namePhoneMap = new HashMap<String, String>();
+    String[] names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contact_list_view = findViewById(R.id.contact_list);
+
+        namePhoneMap = getPhoneNumbers();
+
+        names = new String[namePhoneMap.size()];
+
+        int i=0;
+        for (Map.Entry<String, String> entry : namePhoneMap.entrySet()) {
+            names[i] = entry.getValue()+" -> "+entry.getKey();
+            i++;
+        }
+
+
+        Log.d("Parzival", "Size: "+namePhoneMap.size());
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.contacts_list_item,  names);
+
+        // Assign adapter to ListView
+        contact_list_view.setAdapter(adapter);
+
+
     }
+
+    private Map<String, String> getPhoneNumbers() {
+
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
+
+        // Loop Through All The Numbers
+        while (phones != null && phones.moveToNext()) {
+
+            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            // Cleanup the phone number
+            phoneNumber = phoneNumber.replaceAll("[()\\s-]+", "");
+
+            // Enter Into Hash Map
+            namePhoneMap.put(phoneNumber, name);
+
+        }
+        phones.close();
+
+        return namePhoneMap;
+
+    }
+
 }
