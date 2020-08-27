8abae5db2c4e9d6a79a30fb77acc782d74d2eeb8
bhavyakaria <bhavyakaria@yahoo.in>
2020-08-28 01:53:16 +0530

    completed facebook login code

--- FacebookLogin/app/src/main/java/com/bhavyakaria/facebooklogin/MainActivity.java
+++ FacebookLogin/app/src/main/java/com/bhavyakaria/facebooklogin/MainActivity.java
@@ -2,13 +2,112 @@ package com.bhavyakaria.facebooklogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+    LoginButton loginButton;
+    ImageView imageView;
+    TextView txtUsername, txtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        imageView = findViewById(R.id.imageView);
+        txtUsername = findViewById(R.id.txtUsername);
+        txtEmail = findViewById(R.id.txtEmail);
+
+        boolean loggedOut = AccessToken.getCurrentAccessToken() == null;
+
+        if (!loggedOut) {
+            Log.d("TAG", "Username is: " + Profile.getCurrentProfile().getName());
+            //Using Graph API
+            getUserProfile(AccessToken.getCurrentAccessToken());
+        } else {
+            loginButton.setReadPermissions(Arrays.asList("email", "public_profile"));
+            callbackManager = CallbackManager.Factory.create();
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+            // Callback registration
+            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    // App code
+                    boolean loggedIn = AccessToken.getCurrentAccessToken() == null;
+                    Log.d("API123", loggedIn + " ??");
+                }
+
+                @Override
+                public void onCancel() {
+                    // App code
+                }
+
+                @Override
+                public void onError(FacebookException exception) {
+                    // App code
+                }
+            });
+        }
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void getUserProfile(AccessToken currentAccessToken) {
+        GraphRequest request = GraphRequest.newMeRequest(
+                currentAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.d("TAG", object.toString());
+                        try {
+                            String first_name = object.getString("first_name");
+                            String last_name = object.getString("last_name");
+                            String email = object.getString("email");
+                            String id = object.getString("id");
+                            String image_url = "https://graph.facebook.com/" + id + "/picture?type=normal";
+
+                            txtUsername.setText("First Name: " + first_name + "\nLast Name: " + last_name);
+                            txtEmail.setText(email);
+//                            Picasso.with(MainActivity.this).load(image_url).into(imageView);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "first_name,last_name,email,id");
+        request.setParameters(parameters);
+        request.executeAsync();
     }
 }
